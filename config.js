57eb978e3bb425f6ea7c0527a8866987dc31a69b
kampungtoh <kampungths@gmail.com>
2026-09-21 10:03:06 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,4 @@
+// 奇美醫院 ACLS 課程綱要：使用統計設定
+// 將 Google Apps Script 部署後取得的網址貼在引號內，存檔後上傳到 GitHub。
+// 留空則不記錄任何資料。
+window.ACLS_TRACK_URL = 'https://script.google.com/macros/s/AKfycbwa6UAw-5Z6tSz_2Ch-1c3-Y2wRY9vVyrvNPwM1NCQXleZ3EHwqS_TWgT7HeRuVkg9nag/exec';
